890f193d8042847b641ae78fb10bc021d68aff2f
swoo155 <woosanghee0511@gmail.com>
2017-01-18 20:07:52 +1300

    testing commit

--- www/app/pages/currentStockList/currentStockList.controller.js
+++ www/app/pages/currentStockList/currentStockList.controller.js
@@ -20,37 +20,117 @@
 		 * @description Hook event that is triggered before the view is entered
 		 * @author Nicole Park
 		 */
-		$scope.$on('$ionicView.beforeEnter', function ()
-		{
+		$scope.$on('$ionicView.beforeEnter', function () {
+            _self.groupFilter = '';
 			/*_self.email = null;
 			 _self.password = null;
 			 _self.emailValidationFailed = false;
 			 _self.passwordValidationFailed = false;*/
 
-			_self.products =
-				[
-					{
-						name: 'bby no.1',
-						numbOfStock: 0,
-						group: 'fresh cream'
-					},
-					{
-						name: 'bby no.3',
-						numbOfStock: 0,
-						group: 'fresh cream'
-					}
-				];
+            _self.cakeProducts =
+                [
+                    {
+                        name: 'bby no.1',
+                        numbOfStock: 0,
+                        group: 'Cake',
+                        subgroup: 'Fresh cream'
+                    },
+                    {
+                        name: 'bby no.3',
+                        numbOfStock: 0,
+                        group: 'Cake',
+                        subgroup: 'Fresh cream'
+                    },
+                    {
+                        name: 'choc chif no.2',
+                        numbOfStock: 0,
+                        group: 'Cake',
+                        subgroup: 'Chiffon'
+                    },
+                    {
+                        name: 'gt chif no.3',
+                        numbOfStock: 0,
+                        group: 'Cake',
+                        subgroup: 'Chiffon'
+                    },
+                    {
+                        name: 'fc no.1',
+                        numbOfStock: 0,
+                        group: 'Cake',
+                        subgroup: 'Fresh cream'
+                    },
+                    {
+                        name: 'fc no.3',
+                        numbOfStock: 0,
+                        group: 'Cake',
+                        subgroup: 'Fresh cream'
+                    }
+                ];
 
-
-			_self.pageRefreshClicked = function ()
-			{
-				for (var i = 0; i < _self.products.length; i++)
+            _self.logProducts = [
 				{
-					_self.products[i].numbOfStock = 0;
-				}
-			};
+					name: 'Chocolate',
+					numbOfStock: 0,
+					group: 'Log'
+				},
+                {
+                    name: 'Kumara',
+                    numbOfStock: 0,
+                    group: 'Log'
+                }
+			];
+
+            _self.sliceProducts = [
+				{
+					name: 'Kumara',
+					numbOfStock: 0,
+					group: '6.5'
+				},
+                {
+                    name: 'Kumara',
+                    numbOfStock: 0,
+                    group: '5.2'
+                },
+                {
+                    name: 'Chocolate',
+                    numbOfStock: 0,
+                    group: '6.5'
+                }
+			];
+
+
+            _self.allProducts = _self.cakeProducts.concat(_self.logProducts, _self.sliceProducts);
+
+            console.log(_self.allProducts);
+
+            _self.currentProductList = _self.allProducts;
 		});
 
+
+        _self.pageRefreshClicked = function ()
+        {
+            for (var i = 0; i < _self.products.length; i++)
+            {
+                _self.products[i].numbOfStock = 0;
+            }
+        };
+
+        _self.groupButtonClicked = function (groupFilter)
+        {
+            if(groupFilter == 'Cake')
+			{
+				_self.currentProductList = _self.cakeProducts;
+			}else if(groupFilter == 'Log')
+			{
+                _self.currentProductList = _self.logProducts;
+            } else if(groupFilter == 'Slice')
+            {
+                _self.currentProductList = _self.sliceProducts;
+            }else
+            {
+                _self.currentProductList = _self.allProducts;
+            }
+        };
 
 	}]);
 
---
+++ www/app/shared/productListFilter.js
@@ -0,0 +1,21 @@
+/*
+(function()
+{
+
+    angular.module('shelf').filter('productListFilter', function())
+    {
+
+        return function(unfilteredProducts, group)
+        {
+            if(!group)
+            {
+                return unfilteredProducts;
+            }
+
+            var filteredProducts = [];
+
+
+
+        }
+    }
+})*/
